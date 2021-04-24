53d64743383c94de1671b60d502da95783d4723c
Aura Islamy <74908558+auraislamy@users.noreply.github.com>
2021-04-24 16:35:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,41 +4,36 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 const generateTarget = () => {
-    return Math.floor(Math.random() * 10);
-  }
+  return Math.floor(Math.random() * 10);
+}
 
 const getAbsoluteDistance = (num1, num2) => {
-    return Math.abs(num1 - num2);
+   return Math.abs(num1 - num2);
+}
+
+const compareGuesses = (user, comp, target) => {
+  const userAbs = getAbsoluteDistance(user, target);
+  const compAbs = getAbsoluteDistance(comp, target);
+  if (user < 0 || user > 9){
+    return alert("Your number is out of range.");
+  } 
+  if (userAbs <= compAbs){
+    return true;
+  } else {
+    return false;
   }
-  
-  const compareGuesses = (user, comp, target) => { 
-    if (user < 0 || user > 9){
-      return alert('Your number is out of range');
-    }
-  
-    const absUser = getAbsoluteDistance(user - target);
-    const absComp = getAbsoluteDistance(comp - target);
-    if (absComp >= absUser){
-      return true;
-    } else {
-      return false;
-    }
-  };
-  
-  const updateScore = score => {
-    if (score === 'human'){
-      humanScore ++;
-    }
-    if (score === 'computer'){
-      computerScore ++;
-    }
-  };
-  
-  
-  const advanceRound = () =>{
-    currentRoundNumber ++;
-  };
-  
-  
-  
-  //console.log(compareGuesses(10, 8, 9));
+}
+console.log(compareGuesses(6, 8, 9));
+
+const updateScore = winner => {
+  if (winner === 'human'){
+    humanScore ++;
+  } else if (winner === 'computer'){
+    computerScore ++;
+  }
+}
+
+const advanceRound = () => {
+  currentRoundNumber ++;
+}
+//console.log(currentRoundNumber);
